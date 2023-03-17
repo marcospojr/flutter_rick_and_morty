dec0a1a0635fce468bfe48077002256879065d04
Marcos Jr <marcospojr13@gmail.com>
2023-03-17 14:47:08 -0300

    Details screen and pair

--- lib/details/presentation/widgets/details_widget.dart
+++ lib/details/presentation/widgets/details_widget.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:flutter_rick_and_morty/details/presentation/widgets/character_details_location_widget.dart';
+import 'package:flutter_rick_and_morty/shared/widgets/default_loading_widget.dart';
+import 'package:flutter_rick_and_morty/shared/widgets/default_try_again_widget.dart';
+import '../../../shared/datastructure/pair.dart';
 import '../bloc/character_details_bloc.dart';
 
 class DetailsWidget extends StatelessWidget {
-  const DetailsWidget({Key? key, required String characterId})
-      : super(key: key);
+  const DetailsWidget({
+    Key? key,
+    required this.characterId,
+  }) : super(key: key);
+
+  final String characterId;
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +21,60 @@ class DetailsWidget extends StatelessWidget {
         return Stack(
           children: [
             if (state is CharacterDetailsErrorState) ...{
-              const Text('Ops, error')
+              Center(
+                child: DefaultTryAgainWidget(
+                  onPressed: () => _requestCharacterDetails(context),
+                ),
+              )
             } else if (state is CharacterDetailsResultState) ...{
-              const Text(';)')
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: Image.network(
+                        state.response.image,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                    ...[
+                      Pair('Name', state.response.name),
+                      Pair('Status', state.response.status),
+                      Pair('Species', state.response.species),
+                      Pair('Type', state.response.type),
+                    ].map(
+                      (pair) => ListTile(
+                        title: Text(pair.first),
+                        subtitle: Text(pair.second),
+                      ),
+                    ),
+                    if (state.response.locations.isNotEmpty) ...{
+                      GridView.count(
+                        crossAxisCount: state.response.locations.length,
+                        shrinkWrap: true,
+                        children: state.response.locations
+                            .map(
+                              (currentLocation) =>
+                                  CharacterDetailsLocationWidget(
+                                location: currentLocation,
+                              ),
+                            )
+                            .toList(),
+                      )
+                    }
+                  ],
+                ),
+              )
             } else ...{
-              const Text('Loading')
+              const Center(child: DefaultLoadingWidget())
             }
           ],
         );
       },
     );
   }
+
+  void _requestCharacterDetails(BuildContext context) =>
+      context.read<CharacterDetailsBloc>()
+        ..add(CharacterDetailsRequestEvent(characterId));
 }
